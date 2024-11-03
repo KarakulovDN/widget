fff354f803e8834c1860f799c09359ae55e2ff60
Karakulov Dmitriy <karakulovdn@gmail.com>
2024-11-03 17:10:39 +0700

    финальная правка

--- src/widget.py
+++ src/widget.py
@@ -17,20 +17,18 @@ def mask_account_card(string_bank_card_or_bank_account: str) -> str:
         name_card = "".join("" if el.isdigit() else el for el in string_bank_card_or_bank_account)
         data_card_mask = name_card + number_card_mask
         return data_card_mask
+#
+# print(mask_account_card('Maestro 1596837868705199'))
+# print(mask_account_card('Счет 64686473678894779589'))
+# print(mask_account_card('MasterCard 7158300734726758'))
+# print(mask_account_card('Счет 35383033474447895560'))
+# print(mask_account_card('Visa Classic 6831982476737658'))
+# print(mask_account_card('Visa Platinum 8990922113665229'))
+# print(mask_account_card('Visa Gold 5999414228426353'))
+# print(mask_account_card('Счет 73654108430135874305'))
 
-print(mask_account_card('Maestro 1596837868705199'))
-print(mask_account_card('Счет 64686473678894779589'))
-print(mask_account_card('MasterCard 7158300734726758'))
-print(mask_account_card('Счет 35383033474447895560'))
-print(mask_account_card('Visa Classic 6831982476737658'))
-print(mask_account_card('Visa Platinum 8990922113665229'))
-print(mask_account_card('Visa Gold 5999414228426353'))
-print(mask_account_card('Счет 73654108430135874305'))
-
-
-
-print(get_date('2024-03-11T02:26:18.671407'))
-
+# print(get_date('2024-03-11T02:26:18.671407'))
+#
 
 
 
