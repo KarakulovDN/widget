c3591bbcb92fd7645fc6632fa1b30f29c12e45db
Karakulov Dmitriy <karakulovdn@gmail.com>
2024-11-04 16:36:22 +0700

    home_work_10.1 done 2.0

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,13 @@
-def filter_by_state(state_list: list[dict[str]]) -> list:
+from typing import Union
+
+def filter_by_state(state_list: list) -> Union[list]:
     """Функция, которая принимает список словарей и опционально значение для ключа
     state 'EXECUTED'
     """
     return [x for x in state_list if x.get('state') == 'EXECUTED']
 
 
-def sort_by_date(date_list: list[dict[str]]) -> list:
+def sort_by_date(date_list: list) -> Union[list]:
     """Функция сортировки списка по дате в обратном порядке"""
     return sorted(date_list, key=lambda k: k['date'], reverse=True)
 
